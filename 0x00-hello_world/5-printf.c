0233490fc9b1d997d083cc4224651fb9df1356ab
Keshv18 <veeradukn@gmail>
2022-06-02 12:06:23 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - void
+ *
+ * return - always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
