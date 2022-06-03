bca4d85f13f3e2be0ea5549bee3bdb9e48c3da16
Keshv18 <veeradukn@gmail>
2022-06-03 09:58:17 -0700

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char letter[] = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ";
+        int b;
+
+        for (b = 0; b <= 51; b++);
+        {
+                putchar(letters[b]);
+        }
+        putchar('\n');
+        return (0);
+}
+
