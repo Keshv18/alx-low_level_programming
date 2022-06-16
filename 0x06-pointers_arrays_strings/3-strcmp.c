0a43ddce14652df0faef790068b11cf73a56ff2b
Keshv18 <veeradukn@gmail>
2022-06-16 10:06:10 -0700

    6-pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,22 +5,15 @@
  * @s2: string 2
  * Return: 0 if matching, and ns1 - ns2 if not matching
 */
-
 int _strcmp(char *s1, char *s2)
 {
-int i;
+	int a = 0;
+	int b = 0;
 
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-	if (s1[i] != s2[i])
+	while (s1[a] != '\0' && b == 0)
 	{
-		return (s1[i] - s2[i]);
+		b = s1[a] - s2[a];
+		a++;
 	}
-}
-return (0);
+	return (b);
 }
