5900faa45f8e4b74b33d3ebc52de6c99fe3b95fc
Keshv18 <veeradukn@gmail>
2022-06-20 10:10:14 -0700

    7-pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,22 @@
 #include "main.h"
-
 /**
- * print_chessboard - prints a chessboard
- * @a: array
+ * *print_chessboard - description
+ * @a: board
  * Return: void
  */
+
+
 void print_chessboard(char (*a)[8])
 {
-	int c, r;
+int i, j;
 
-	for (r = 0; r < 8; r++)
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[r][c]);
-		}
-		_putchar('\n');
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
+
 }
