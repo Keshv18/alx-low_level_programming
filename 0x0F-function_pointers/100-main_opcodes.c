9c322c6387e498e57a3eeb3b89cc20a0ae20287f
Keshv18 <veeradukn@gmail>
2022-07-06 08:10:13 -0700

    oxof

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,35 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the opcodes
- * @argc: argument counter
- * @argv: argument vector
+ * main - prints opcode of own main function
  *
- * Return: Always 0 (Sucess)
+ * @argc: argument count
+ * @argv: arg value
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
+	int x, i;
+	unsigned char *p;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+	x = atoi(argv[1]);
+	if (x < 0)
 	{
 		printf("Error\n");
 		exit(2);
+	}
+	p = (unsigned char *)main;
+	i = 0;
+	if (x > 0)
+	{
+		while (i < (x - 1))
+			printf("%02hhx ", p[i++]);
+		printf("%hhx\n", p[i]);
 	}
 	return (0);
 }
